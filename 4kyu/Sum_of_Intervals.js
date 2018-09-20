54176705a771fe11bd1dc66c5d49d922be73e99f
J.A <sky61054@gmail.com>
2018-09-20 15:24:49 +0800

    Rename variable

--- 4kyu/Sum_of_Intervals.js
+++ 4kyu/Sum_of_Intervals.js
@@ -5,27 +5,29 @@ function sumIntervals(intervals) {
     if (records.length == 0) {
       records.push(examinee);
     } else {
-      for (var item2 of records) {
-        frontNum = item2[0];
-        endNum = item2[1];
+      for (var examiner of records) {
+        examinerFrontNum = examiner[0];
+        examinerEndNum = examiner[1];
+
+        examineeFrontNum = examinee[0];
+        examineeEndNum = examinee[1];
         if (
-          frontNum < examinee[0] &&
-          examinee[0] < endNum &&
-          examinee[1] >= endNum
+          examinerFrontNum < examineeFrontNum &&
+          examineeFrontNum < examinerEndNum &&
+          examineeEndNum >= examinerEndNum
         ) {
-          item2[1] = examinee[1];
-          break;
-        } else if (frontNum > examinee[0]) {
+          examiner[1] = examineeEndNum;
+        } else if (examinerFrontNum > examineeFrontNum) {
           records.push(examinee);
-          break;
-        } else if (examinee[1] < endNum) {
-          break;
-        } else if (frontNum == examinee[0] && examinee[1] == endNum) {
-          break;
+        } else if (examineeEndNum < examinerEndNum) {
+        } else if (
+          examinerFrontNum == examineeFrontNum &&
+          examineeEndNum == examinerEndNum
+        ) {
         } else {
           records.push(examinee);
-          break;
         }
+        break;
       }
     }
   });
@@ -37,10 +39,10 @@ function sumIntervals(intervals) {
   return result;
 }
 
-//console.log(sumIntervals([[1, 4], [3, 5], [7, 10]]));
-// console.log(sumIntervals([[1, 4], [7, 10], [3, 5]]));
-// console.log(sumIntervals([[1, 20]]));
-// console.log(sumIntervals([[1, 5], [1, 5]]));
-//console.log(sumIntervals([[1, 5], [7, 10], [3, 5]]));
-//console.log(sumIntervals([[1, 20], [10, 20], [1, 6], [16, 19], [5, 11]]));
+console.log(sumIntervals([[1, 4], [3, 5], [7, 10]]));
+console.log(sumIntervals([[1, 4], [7, 10], [3, 5]]));
+console.log(sumIntervals([[1, 20]]));
+console.log(sumIntervals([[1, 5], [1, 5]]));
+console.log(sumIntervals([[1, 5], [7, 10], [3, 5]]));
+console.log(sumIntervals([[1, 20], [10, 20], [1, 6], [16, 19], [5, 11]]));
 console.log(sumIntervals([[11, 15], [6, 10], [1, 2]]));
